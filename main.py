31600c93bd27a24b111f79ee84294e8c4d8bffb0
Sarthak Singh <105309861+sarthakk1890@users.noreply.github.com>
2023-06-28 15:15:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ async def predict(file: UploadFile = File(...)):
     return {"result": result}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
